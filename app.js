22f0e300162e4796e4773998f891809ebc15a9c8
adharikrishnan <adharikrishnan456@gmail.com>
2023-03-11 23:25:38 +0530

    added main functionality

--- app.js
+++ app.js
@@ -10,21 +10,41 @@ function UI(){}
 
 UI.prototype.addBook = function(book) {
     const list = document.getElementById('book-list');
-    
+    const row = document.createElement('tr');
+
+    for(const detail in book){
+        const entry = document.createElement('td');
+        entry.appendChild(document.createTextNode(book[detail]));
+        row.appendChild(entry);
+    }
+    row.innerHTML += '<td><a href ="#" class="delete">X</td>';
+
+    list.appendChild(row);
 }
 
+UI.prototype.clearFields = function(book){
 
-document.getElementById('book-form').addEventListener('submit', function(e){
-    const title = document.getElementById('title').value,
-          author = document.getElementsById('author').value,
-          isbn = document.getElementById('isbn').value,
-          year = document.getElementById('year').value;
+    for(const attr in book){
+        document.getElementById(attr).value = '';
+    }
+    console.log('cleared');
+}
 
+document.getElementById('book-form').addEventListener('submit', 
+ function(e){
+
+    const title = document.getElementById('title').value;
+        author = document.getElementById('author').value;
+        isbn = document.getElementById('isbn').value,
+        year = document.getElementById('year').value
+ 
     const book = new Book(title, author, isbn, year);
 
-    const UI = new UI();
+    const interface = new UI();
 
+    interface.addBook(book);
 
+    interface.clearFields(book);
 
     e.preventDefault();
 })
